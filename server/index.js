16479a71566b71b0e15f05950988c3b106011f70
Abhishek Singh <abhiwebdev2718@gmail.com>
2024-12-12 12:41:03 +0530

    setup of api

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import fetch from "node-fetch";
+import express from "express";
+const port = process.env.PORT || 8000,
+  app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Welcome to the RN server!",
+  });
+});
+
+app.get("/api/users", async (req, res) => {
+  const query = req.query;
+  const limit = req.query?.limit ? req.query.limit : 10;
+  const page = req.query?.page ? req.query.page : 1;
+  const url = `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}&_limit=${limit}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  res.status(200).json({
+    data,
+    success: true,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`RN server listening on ${port}`);
+});
